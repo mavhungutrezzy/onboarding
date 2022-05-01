88deaa1057fd13de880a7107f83b1a2996d08a61
mavhungutrezzy <mavhungutrezzy@gmail.com>
2022-05-01 21:09:42 +0200

    created pageview widget to display onboarding screen

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,207 +1,54 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:js';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'package:onboarding/content_model.dart';
 
-class OnBoarding extends StatelessWidget {
-  const OnBoarding({Key? key}) : super(key: key);
+
+class OnBoarding extends StatefulWidget {
+  OnBoarding({Key? key}) : super(key: key);
 
   @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  @override
   Widget build(BuildContext context) {
-    return GetStarted(context);
-  }
+    return Scaffold(
+      body: PageView.builder(
+        itemCount: contents.length,
+        itemBuilder: (_, i) {
+          return Padding(
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              children: [
+                Image.asset(contents[i].image),
+                Text(
+                  contents[i].title,
+                  style: GoogleFonts.bebasNeue(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
 
-  Widget OnBoarding1(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.asset(
-            'assets/droll-woman-with-delivered-gift.png',
-            height: size.height * 0.45,
-          ),
-          Text(
-            'Get Inspired',
-            style: GoogleFonts.bebasNeue(
-              fontSize: 45,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 40.0),
-            child: Container(
-              alignment: Alignment.center,
-              child: Text(
-                "Dont know what to eat? Take a picture, we'\ll suggest things to cook with your ingredients",
-                style: GoogleFonts.roboto(
-                  fontSize: 20,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-          Text(''),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 30.0),
-            child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Skip',
-                    style: GoogleFonts.roboto(
-                      fontSize: 15,
-                    ),
                   ),
-                  Container(
-                    child: Row(
-                      children: [
-                        Text(
-                          'Next',
-                          style: GoogleFonts.roboto(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Icon(Icons.arrow_right_alt)
-                      ]
-                    )
-                  )
-                ]
-              )
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget OnBoarding2(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 30.0),
-            child: Image.asset(
-              'assets/droll-vacation.png',
-              height: size.height * 0.45,
-            ),
-          ),
-          Text(
-            'Easy & Healthy',
-            style: GoogleFonts.bebasNeue(
-              fontSize: 45,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 40.0),
-            child: Container(
-              alignment: Alignment.center,
-              child: Text(
-                "Find thousand of easy and healthy recipes so you save time and eat better.",
-                style: GoogleFonts.roboto(
-                  fontSize: 20,
                 ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-          Text(''),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 30.0),
-            child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Skip',
-                    style: GoogleFonts.roboto(
-                      fontSize: 15,
-                    ),
+                SizedBox(height: 50,),
+                Text(
+                  contents[i].description,
+                  style: GoogleFonts.roboto(
+                    fontSize: 18,
+                    color: Colors.grey,
                   ),
-                  Container(
-                    child: Row(
-                      children: [
-                        Text(
-                          'Next',
-                          style: GoogleFonts.roboto(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Icon(Icons.arrow_right_alt)
-                      ]
-                    )
-                  )
-                ]
-              )
-            ),
-          )
-        ],
-      ),
-    );
-  }
 
-   Widget GetStarted(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 30.0),
-            child: Image.asset(
-              'assets/droll-project-launch.png',
-              height: size.height * 0.45,
-            ),
-          ),
-          Text(
-            'Save your favorites',
-            style: GoogleFonts.bebasNeue(
-              fontSize: 45,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 40.0),
-            child: Container(
-              alignment: Alignment.center,
-              child: Text(
-                "Save you favorite recipes and get reminders to buy the ingredients to cook them",
-                style: GoogleFonts.roboto(
-                  fontSize: 20,
                 ),
-                textAlign: TextAlign.center,
-              ),
+              ],
             ),
-          ),
-          Text(''),
-        
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 120, vertical: 12),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.black,
-            ),
-            child: Text(
-              'Get Started',
-              style: GoogleFonts.bebasNeue(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              )
-            )
-          ),
-      
-        ],
+          );
+        },
       ),
     );
   }
-
-
 }
